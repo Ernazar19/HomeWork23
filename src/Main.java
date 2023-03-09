426350bc44c676bec788d098b45fc0173da8fa5e
ernazaresanbekov <ernazar.03@icloud.com>
2023-03-09 15:38:30 +0600

    HomeWork23.iml

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Random random = new Random();
+        ArrayList<Integer> list = new ArrayList<>();
+        ArrayList<Integer>list1 = new ArrayList<>();
+        for (int i = 0; i <=50 ; i++) {
+            list.add(random.nextInt(1,100));
+            System.out.print(" "+list.get(i));
+        }
+        ArrayList<Integer>list2 = new ArrayList<>();
+        for (int i = 0; i <list.size() ; i++) {
+            if (list.get(i)%2==0){
+                list1.add(i);
+            } else if (list.get(i)%2==1) {
+                list2.add(i);
+            }
+        }
+        System.out.println("\n~~~~jup~~~~"+list1);
+        System.out.println("~~~~tak~~~~"+list2);
     }
 }
